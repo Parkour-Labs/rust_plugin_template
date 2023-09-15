4c04fe0d1cf3e2ea438ac8b4a3f6628eabada135
bridgekat <zhanrong.qiao21@imperial.ac.uk>
2023-09-15 10:02:19 +0100

    fix(ci): try again

--- example/integration_test/integration_test.dart
+++ example/integration_test/integration_test.dart
@@ -1,10 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 import 'package:theprojectname/native_bindings.dart';
 import 'package:theprojectname/native_library.dart';
 
 /// These tests must be run with native binaries bundled alongside.
 /// This can be done with `flutter test integration_test`.
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   test('function call', () {
     assert(NativeBindings(getNativeLibrary()).add(2, 3) == 5);
   });
